844a0c0e154a9cfe1c5de643635639a401788e14
Mizo-G <moataz.g.developer@gmail.com>
2024-08-13 22:33:09 +0300

    removedunwanted folders

--- obj/Debug/net8.0/TestDapper.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestDapper.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestDapper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b3c86ff458ab51954ba7477f413bf3f595b10b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestDapper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestDapper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
